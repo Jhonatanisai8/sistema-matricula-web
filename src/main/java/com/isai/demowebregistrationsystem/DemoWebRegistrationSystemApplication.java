94fe68c9c8889b4b14a34d0ebf06acb5e52fe94b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-28 21:43:24 -0500

    init

---
+++ src/main/java/com/isai/demowebregistrationsystem/DemoWebRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demowebregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebRegistrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebRegistrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/demowebregistrationsystem/DemoWebRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demowebregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebRegistrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
