73e17f00c74ab4377059a4c824af6a3e07c9414f
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-02 14:49:16 -0500

    funcionalidad para guardar un apoderado

---
+++ src/main/java/com/isai/demowebregistrationsystem/repositorys/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.isai.demowebregistrationsystem.repositorys;
+
+import com.isai.demowebregistrationsystem.model.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByUserName(String userName);
+}
