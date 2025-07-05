bed9d4d44109857998065e3ef7206b1c5b4a39d5
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-05 03:16:46 -0500

    cambios para poder visualizar los hijos por parte del apoderado

---
+++ src/main/java/com/isai/demowebregistrationsystem/model/dtos/apoderado/EstudianteListaApoderadoDTO.java
@@ -0,0 +1,32 @@
+package com.isai.demowebregistrationsystem.model.dtos.apoderado;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EstudianteListaApoderadoDTO {
+
+    private Integer idEstudiante;
+
+    private String codigoEstudiante;
+
+    private String nombresCompletos;
+
+    private String dni;
+
+    private LocalDate fechaNacimiento;
+
+    private String emailEducativo;
+
+    private String telefonoEmergencia;
+
+    private Boolean seguroEscolar;
+
+    private String parentescoConApoderadoPrincipal;
+}
